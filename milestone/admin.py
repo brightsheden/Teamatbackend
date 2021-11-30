16ce52ad9c9005e082473a03884e7e8e4d02746f
shedenbright <shedenbright@gmail.com>
2021-11-29 16:27:47 -0800

    firdt comit

---
+++ milestone/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Blog)
+admin.site.register(AgricProduct)
+admin.site.register(NaturalProduct)
+admin.site.register(InformationTechProduct)
+admin.site.register(Booking)
+admin.site.register(NewsLetter)
+admin.site.register(ReviewNatural)
+admin.site.register(ReviewAgric)
+admin.site.register(ReviewIct)
+admin.site.register(Promotion)
+admin.site.register(ReviewBlog)
---
+++ milestone/serializer.py
@@ -0,0 +1,109 @@
+
+from rest_framework import serializers
+from .models import *
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User 
+        fields = ['id',"_id",'username','name', 'email',"isAdmin"]
+
+    def get_name(self,obj):
+        name= obj.first_name
+        if name == "":
+            name = obj.email
+        return name
+
+    def get__id(self,obj):
+        return obj.id
+    
+    def get_isAdmin(self,obj):
+        return obj.is_staff
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username','email',"isAdmin",'name', 'token']
+
+    def get_token(self, obj):
+        token =RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+#blog serializer
+class BlogSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Blog
+        fields ="__all__"
+
+    def get_reviews(self,obj):
+        reviews= obj.reviewblog_set.all()
+        serializer = ReviewBlogSerializer(reviews, many=True)
+        return serializer.data
+
+
+class ReviewBlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewBlog
+        fields = "__all__"
+
+
+class AgricProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= AgricProduct
+        fields ="__all__"
+
+    def get_reviews(self,obj):
+        reviews= obj.review_set.all()
+        serializer = ReviewAgricSerializer(reviews, many=True)
+        return serializer.data
+
+class ReviewAgricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewAgric
+        fields = "__all__"
+
+
+class NaturalProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= NaturalProduct
+        fields ="__all__"
+    def get_reviews(self,obj):
+        reviews= obj.review_set.all()
+        serializer = ReviewNaturalSerializer(reviews, many=True)
+        return serializer.data
+
+class ReviewNaturalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewNatural
+        fields = "__all__"
+
+
+class InformationTechProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= InformationTechProduct
+        fields ="__all__"
+    def get_reviews(self,obj):
+        reviews= obj.review_set.all()
+        serializer = ReviewInformationTechSerializer(reviews, many=True)
+        return serializer.data
+
+class ReviewInformationTechSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewIct
+        fields = "__all__"
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
+
+class NewsLetterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsLetter
+        fields = "__all__"
