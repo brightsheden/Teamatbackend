b49abce2131c6382a9ed05a11738601536d69800
brightsheden <69737311+brightsheden@users.noreply.github.com>
2021-12-09 08:04:09 +0100

    Update settings.py
    Removing allow host

--- milestonebackend/settings.py
+++ milestonebackend/settings.py
@@ -97,12 +97,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
+#CORS_ALLOWED_ORIGINS = [
     
 
-    "http://localhost:3000",
+   # "http://localhost:3000",
     
-]
+#]
 CORS_ALLOW_ALL_ORIGINS: True
 
 
